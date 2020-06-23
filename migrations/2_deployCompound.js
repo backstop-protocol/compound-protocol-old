4ce8a66cd9867b06130f962b103759e7e19af74b
Jitendra Chittoda <jcchittoda@gmail.com>
2020-06-23 16:11:42 +0530

    Validating mint and redeem of ETH in migration scripts

--- migrations/2_deployCompound.js
+++ migrations/2_deployCompound.js
@@ -14,8 +14,11 @@ const StandardToken = artifacts.require("StandardToken");
 const CErc20 = artifacts.require("CErc20");
 
 const BN = web3.utils.BN;
+var assert = require("chai").assert;
+var expect = require("chai").expect;
 
 module.exports = async function (deployer, network, accounts) {
+  const ZERO = new BN(0);
   const UNIT = new BN(10).pow(new BN(18)); // 1e18
   const ONE_DAY = 24 * 60 * 60;
   const THREE_DAYS = 3 * ONE_DAY;
@@ -60,7 +63,6 @@ module.exports = async function (deployer, network, accounts) {
 
   const initialExchangeRateMantissa = UNIT;
   // Deploy cETH
-
   const cETH = await deployer.deploy(
     CEther,
     comptroller.address,
@@ -75,19 +77,17 @@ module.exports = async function (deployer, network, accounts) {
   // Deploy Maximillion
   const maximillion = await deployer.deploy(Maximillion, cETH.address);
 
-  // Deploying ERC20 tokens
+  // Deploying ZRX ERC20 tokens
   const INIT_SUPPLY = new BN(1000000);
-  const name = "0x";
-  const symbol = "ZRX";
   const decimals = 18;
   const ONE_ZRX_TOKEN = new BN(10).pow(new BN(decimals));
   const initSupplyInTokens = INIT_SUPPLY.mul(ONE_ZRX_TOKEN);
   const ZRX = await deployer.deploy(
     StandardToken,
     initSupplyInTokens,
-    name,
+    "0x",
     decimals,
-    symbol
+    "ZRX"
   );
 
   // Deploy cZRX
@@ -103,6 +103,35 @@ module.exports = async function (deployer, network, accounts) {
 
   //await deployer.deploy(PriceOracleProxy);
 
+  console.log("\nAdding cTokens to Market...");
+  // Adding cETH to Market
+  await comptroller._supportMarket(cETH.address);
+  let result = await comptroller.markets(cETH.address);
+  let isListed = result[0];
+  assert(isListed, "ERROR: cETH not listed");
+
+  // Adding cZRX to Market
+  await comptroller._supportMarket(cZRX.address);
+  result = await comptroller.markets(cZRX.address);
+  isListed = result[0];
+  assert(isListed, "ERROR: cZRX not listed");
+
+  console.log("\nValidating setup...");
+  // Minting cETH with ETH
+  await cETH.mint({ value: web3.utils.toWei("1", "ether") });
+  let ethBalOfcETHContract = await web3.eth.getBalance(cETH.address);
+  assert(UNIT.eq(new BN(ethBalOfcETHContract)));
+  let cETHBalOfUser = await cETH.balanceOf(admin);
+  assert(UNIT.eq(cETHBalOfUser));
+
+  // Redeem ETH with cETH
+  await cETH.redeem(cETHBalOfUser);
+  ethBalOfcETHContract = await web3.eth.getBalance(cETH.address);
+  assert(ZERO.eq(new BN(ethBalOfcETHContract)));
+  cETHBalOfUser = await cETH.balanceOf(admin);
+  assert(ZERO.eq(cETHBalOfUser));
+
+  console.log("Validation OK\n");
   console.log("=======================");
   console.log("Compound Base Contracts");
   console.log("=======================");
